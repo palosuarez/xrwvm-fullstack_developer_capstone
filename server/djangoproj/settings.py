0afc985e97352b7e357bf1409abde5c220bab931
palosuarez <palosuarez@users.noreply.github.com>
2026-03-22 15:36:29 -0400

    feat: module 2 — login/logout/register views, React Register component, frontend build

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend/static')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,5 +136,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'frontend/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static')]
 CSRF_TRUSTED_ORIGINS=['https://palosuarez-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
